649e1417ca698b2462c008da6cd2b98667c22518
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-10-24 19:59:55 +0700

    feature: add navigation in main

--- Week 5/LoginFlow/app/src/main/java/vn/edu/ut/hieupm9898/loginflow/MainActivity.kt
+++ Week 5/LoginFlow/app/src/main/java/vn/edu/ut/hieupm9898/loginflow/MainActivity.kt
@@ -3,27 +3,28 @@ package vn.edu.ut.hieupm9898.loginflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import vn.edu.ut.hieupm9898.loginflow.ui.screens.LoginScreen
+import vn.edu.ut.hieupm9898.loginflow.ui.screens.ProfileScreen
 import vn.edu.ut.hieupm9898.loginflow.ui.theme.LoginFlowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LoginFlowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
                 }
             }
         }
@@ -31,17 +32,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoginFlowTheme {
-        Greeting("Android")
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "login") {
+        composable(route = "login") {
+            LoginScreen(navController = navController)
+        }
+        composable(route = "profile") {
+            ProfileScreen(navController = navController)
+        }
     }
 }
