d269e269788b5f389234d4cd7ba1ad0ac9b0eaa8
Minhhieu3012 <hieupm9898@ut.edu.vn>
2025-10-24 14:54:12 +0700

    feature: ui login screen

--- Week 5/LoginFlow/app/src/main/java/vn/edu/ut/hieupm9898/loginflow/ui/screens/LoginScreen.kt
+++ Week 5/LoginFlow/app/src/main/java/vn/edu/ut/hieupm9898/loginflow/ui/screens/LoginScreen.kt
@@ -1,2 +1,138 @@
 package vn.edu.ut.hieupm9898.loginflow.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import vn.edu.ut.hieupm9898.loginflow.R
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Scaffold(
+        bottomBar = {
+            Text(
+                text = "© UTHSmartTasks",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp),
+                fontSize = 18.sp,
+                color = Color(0xFF4A4646)
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top // sap xep tu tren xuong
+        ) {
+            Spacer(modifier = Modifier.height(80.dp))
+
+            // logo uth
+            Image(
+                painter = painterResource(id = R.drawable.uth_logo),
+                contentDescription = "UTH Logo",
+                modifier = Modifier.size(250.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Ten ung dung
+            Text(
+                text = "SmartTasks",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF2196F3)
+            )
+            Text(
+                text = "A simple and efficient to-do app",
+                fontSize = 14.sp,
+                color = Color(0xFF2196F3)
+            )
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            // welcome
+            Text(
+                text = "Welcome",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFF333333)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Ready to explore? Log in to get started.",
+                fontSize = 16.sp,
+                color = Color(0xFF4A4646),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            // nut dang nhap
+            OutlinedButton(
+                onClick = {
+                    navController.navigate("profile")
+                },
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .height(60.dp),
+                shape = RoundedCornerShape(10.dp), // bo tron
+                border = BorderStroke(1.dp, Color.White),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFD5EDFF)
+                )
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_google_logo),
+                        contentDescription = "Google Logo",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Text(
+                        text = "SIGN IN WITH GOOGLE",
+                        fontWeight = FontWeight(weight = 600),
+                        color = Color(0xFF130160)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(navController = rememberNavController())
+}
